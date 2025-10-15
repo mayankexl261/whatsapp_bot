634ab008bf5981c68766e6cc10b00a7f23ebbdc8
mayankexl261 <Mayank.Kakkar@exlservice.com>
2025-10-15 23:40:14 +0530

    Add files via upload

---
+++ start_script.sh
@@ -0,0 +1,14 @@
+sudo apt update -y
+sudo apt-get update -y
+
+sudo apt-get install -y python3 python3-pip python3.12-venv
+
+if [ ! -d "myenv" ]; then
+  python3 -m venv myenv
+fi
+
+source myenv/bin/activate
+
+pip install --upgrade pip
+
+pip install fastapi uvicorn nest-asyncio pyngrok twilio pandas sentence-transformers faiss-cpu
